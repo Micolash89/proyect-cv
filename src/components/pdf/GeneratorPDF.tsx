e00848eb81a541113cd21a88f0dbec1e63ca6ce8
javier <espindolajavier2013@gmail.com>
2024-11-03 15:18:50 -0300

    "GeneradorPDF.tsx actualizado: código reformateado, nuevas propiedades añadidas a la interfaz CVData y cambios de estilo menores".

--- src/components/pdf/GeneratorPDF.tsx
+++ src/components/pdf/GeneratorPDF.tsx
@@ -1,9 +1,15 @@
-
-import { Document, Page, Text, View, StyleSheet, Font } from '@react-pdf/renderer';
-import { TypeIAData } from '../PreviewCV';
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
+import { TypeIAData } from "../PreviewCV";
 
 Font.register({
-  family: 'Times New Roman',
+  family: "Times New Roman",
   src: "/fonts/TimesNewRoman.ttf",
 });
 
@@ -15,85 +21,120 @@ interface CVData {
   phone: string;
   ciudad: string;
   provincia: string;
-  education: Array<{ carrera: string; estado: string; estudios: string;institucion:string; anioInicioEducacion:string;anioFinEducacion:string;  }>;
-  experience: Array<{ nombreEmpresa: string; puesto: string; anioInicioExperiencia: string; anioFinExperiencia: string; descripcionExperiencia: string }>;
-  cursos: Array<{ curso: string; institucion: string; anioInicioCurso: string }>;
+  education: Array<{
+    carrera: string;
+    estado: string;
+    estudios: string;
+    institucion: string;
+    zonaEducacion: string;
+    anioInicioEducacion: string;
+    anioFinEducacion: string;
+  }>;
+  experience: Array<{
+    nombreEmpresa: string;
+    puesto: string;
+    zonaEmpresa: string;
+    anioInicioExperiencia: string;
+    anioFinExperiencia: string;
+    descripcionExperiencia: string;
+  }>;
+  cursos: Array<{
+    curso: string;
+    institucion: string;
+    anioInicioCurso: string;
+  }>;
 }
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'column',
-    backgroundColor: '#FFFFFF',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    backgroundColor: "#FFFFFF",
     padding: 10,
-    fontFamily: 'Times New Roman',
+    fontFamily: "Times New Roman",
   },
   section: {
     margin: 10,
     padding: 10,
   },
   section2: {
-   borderTop:'1px solid black',
- 
+    borderBottom: "1px solid black",
   },
   title: {
     fontSize: 24,
-    marginBottom: 5,
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    textAlign: 'center'
+    marginBottom: 2,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    textAlign: "center",
   },
   header: {
     fontSize: 18,
-    marginBottom: 10,
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    textAlign: 'center'
+    marginBottom: 5,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    textAlign: "center",
+    borderBottom: "1px solid black",
   },
   subHeader: {
     fontSize: 14,
     marginBottom: 5,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   text: {
     fontSize: 11,
-    marginBottom: 5,
+    marginBottom: 3,
+    marginTop: 3,
+  },
+  profile:{
+    fontStyle: "italic",
   },
   bold: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   centerText: {
-    textAlign: 'center',
+    textAlign: "center",
+    width: "100%",
   },
 });
 
 // Componente principal
-export const MyDocumentPDF: React.FC<{ cvData: CVData, iaData:TypeIAData }> = ({ cvData, iaData }) => (
+export const MyDocumentPDF: React.FC<{
+  cvData: CVData;
+  iaData: TypeIAData;
+}> = ({ cvData, iaData }) => (
   <Document>
     <Page size="A4" style={styles.page}>
       {/* Encabezado */}
       <View style={styles.section}>
         <Text style={styles.title}>{`${cvData.name} ${cvData.lastName}`}</Text>
-      <View style={styles.section2}>
-        <Text style={styles.text}>{cvData.email} | {cvData.phone} | {cvData.ciudad}, {cvData.provincia} | {cvData.fechaNacimiento}</Text>
-      </View>
-      <View style={styles.section2}>
-        <Text style={styles.text}>{iaData.profile}</Text>
-      </View>
-        
+        <View style={styles.section2}>
+          <Text style={styles.text}>
+            {cvData.ciudad}, {cvData.provincia} | {cvData.email} |{" "}
+            {cvData.phone} | {cvData.fechaNacimiento}
+          </Text>
+        </View>
+        <View style={styles.profile}>
+          <Text style={styles.text}>{iaData.profile}</Text>
+        </View>
       </View>
 
       {/* Educación */}
       <View style={styles.section}>
         <Text style={styles.header}>Educación</Text>
-      <View style={[styles.section2]}>
-        {cvData.education.map((edu, index) => (
-          <View key={index} style={{ marginBottom: 5 }}>
-            <Text style={styles.subHeader}>{edu.institucion}</Text>
-            <Text style={styles.subHeader}>{edu.carrera}</Text>
-            <Text style={styles.text}>{`${edu.estudios}, ${edu.estado}`}</Text>
-            <Text style={styles.text}>{`${edu.anioInicioEducacion}, ${edu.anioFinEducacion}`}</Text>
-          </View>
-        ))}
+        <View >
+          {cvData.education.map((edu, index) => (
+            <View key={index} style={{ marginBottom: 5 }}>
+              <Text style={styles.subHeader}>{edu.institucion}</Text>
+              <Text style={styles.subHeader}>{edu.carrera}</Text>
+              <Text
+                style={styles.text}
+              >{`${edu.estudios}, ${edu.estado}`}</Text>
+              <Text
+                style={styles.text}
+              >{`${edu.anioInicioEducacion}, ${edu.anioFinEducacion}`}</Text>
+            </View>
+          ))}
         </View>
       </View>
 
@@ -103,8 +144,12 @@ export const MyDocumentPDF: React.FC<{ cvData: CVData, iaData:TypeIAData }> = ({
         {cvData.experience.map((exp, index) => (
           <View key={index} style={{ marginBottom: 10 }}>
             <Text style={styles.subHeader}>{exp.puesto}</Text>
-            <Text style={styles.text}>{`${exp.nombreEmpresa}, ${exp.anioInicioExperiencia} - ${exp.anioFinExperiencia}`}</Text>
-            <Text style={styles.text}>{iaData.descriptionWork.split('\n')[index]}</Text>
+            <Text
+              style={styles.text}
+            >{`${exp.nombreEmpresa}, ${exp.anioInicioExperiencia} - ${exp.anioFinExperiencia}`}</Text>
+            <Text style={styles.text}>
+              {iaData.descriptionWork.split("\n")[index]}
+            </Text>
           </View>
         ))}
       </View>
@@ -121,11 +166,10 @@ export const MyDocumentPDF: React.FC<{ cvData: CVData, iaData:TypeIAData }> = ({
 
       {/*Skills*/}
       <View style={styles.section}>
-      <Text style={styles.header}>Skills</Text>
-      <View style={styles.section2}>
-        <Text style={styles.text}>{iaData.skills}</Text>
-      </View>
-        
+        <Text style={styles.header}>Habilidades</Text>
+        <View >
+          <Text style={styles.text}>{iaData.skills}</Text>
+        </View>
       </View>
     </Page>
   </Document>

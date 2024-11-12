fd683208dbadfcbcf926fe1256dc93c3b6d4bd60
javier <espindolajavier2013@gmail.com>
2024-11-12 11:29:09 -0300

    "Actualizar los nombres e identificadores de los campos de entrada en el componente FormRegister"

--- src/components/FormRegister.tsx
+++ src/components/FormRegister.tsx
@@ -490,14 +490,14 @@ function FormRegister({
                   <input
                     type="text"
                     name="institucion"
-                    id="institucion"
+                    id="floating_institucion"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-500 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     value={newEducation.institucion}
                     onChange={handleEducationChange}
                   />
                   <label
-                    htmlFor="institucion"
+                    htmlFor="floating_institucion"
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     Nombre de la institución
@@ -507,8 +507,8 @@ function FormRegister({
                 <div className="relative z-0 w-full  group ">
                   <input
                     type="text"
-                    name="carrera_titulo"
-                    id="carrera"
+                    name="carrera"
+                    id="carrera_titulo"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-500 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
                     value={newEducation.carrera}
